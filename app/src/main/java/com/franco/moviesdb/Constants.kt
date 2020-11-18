87ce600242e12a7da0cf21aa9babb78cd3c45b61
franco <franquicidad@gmail.com>
2020-11-17 21:54:33 -0500

    Added retrofit succesfully and retreived MoviesActionList

--- app/src/main/java/com/franco/moviesdb/Constants.kt
+++ app/src/main/java/com/franco/moviesdb/Constants.kt
@@ -1,2 +1,24 @@
 package com.franco.moviesdb
 
+const val BASE_URL = "https://api.themoviedb.org/3/discover/"
+const val APPEND_API = "?api_key=7d51874568317dfd0c91db399be2bdec"
+const val IMAGE_URL = "http://image.tmdb.org/t/p/w342"
+
+const val ALONE_API = "7d51874568317dfd0c91db399be2bdec"
+const val APPEND_TO_SELECT_GENRES = "&with_genres="
+const val BASE_URL_TV_POPULAR = "https://api.themoviedb.org/3/tv/popular?api_key="
+const val APPEND_MOVIE = "movie"
+const val APPEND_TV = "tv"
+
+const val ACTION = 28
+const val COMEDY = 35
+
+const val MOVIE_URL_ACTION = BASE_URL + APPEND_MOVIE + APPEND_API + APPEND_TO_SELECT_GENRES + ACTION
+const val MOVIE_URL_COMEDY = BASE_URL + APPEND_MOVIE + APPEND_API + APPEND_TO_SELECT_GENRES + COMEDY
+
+const val TV_URL_ACTION = BASE_URL + APPEND_TV + APPEND_API + APPEND_TO_SELECT_GENRES + ACTION
+const val TV_URL_COMEDY = BASE_URL + APPEND_TV + APPEND_API + APPEND_TO_SELECT_GENRES + COMEDY
+//https://api.themoviedb.org/3/tv/popular?api_key=7d51874568317dfd0c91db399be2bdec&page=1
+
+//https://api.themoviedb.org/3/discover/movie?api_key=7d51874568317dfd0c91db399be2bdec&with_genres=28
+//https://api.themoviedb.org/3/discover/tv?api_key=7d51874568317dfd0c91db399be2bdec&with_genres=28
--- app/src/main/java/com/franco/moviesdb/ui/home/HomeFragment.kt
+++ app/src/main/java/com/franco/moviesdb/ui/home/HomeFragment.kt
@@ -9,10 +9,14 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.franco.moviesdb.R
+import com.franco.moviesdb.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -21,11 +25,14 @@ class HomeFragment : Fragment() {
     ): View? {
         homeViewModel =
                 ViewModelProvider(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+//        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+//        val textView: TextView = root.findViewById(R.id.text_home)
+
+
+        homeViewModel.response.observe(viewLifecycleOwner, Observer {
+            val moviesActionList = it.movieActionList
         })
-        return root
+        return binding.root
     }
 }
