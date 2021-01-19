86a1ca207af8a564f44a075ba733414fb3890e5d
Felipe Franco <Felipe Franco>
2021-01-18 23:19:03 -0500

    completing actors profile 2

---
+++ app/src/main/java/com/franco/moviesdb/network/entityMappers/MoviesByActorMapper.kt
@@ -0,0 +1,31 @@
+package com.franco.moviesdb.network.entityMappers
+
+import com.franco.moviesdb.domain.Actor
+import com.franco.moviesdb.database.actors.model.ActorListMovies as Database
+import com.franco.moviesdb.network.model.ActorListMovies as Network
+import com.franco.moviesdb.domain.ActorListMovies
+
+fun Database.fromDatabaseToDomain(): ActorListMovies =
+        ActorListMovies(
+                id, backdropPath, character, originalLanguage, overview, posterPath, releaseDate
+                ?: "", title, rating
+        )
+
+fun ActorListMovies.fromDomainToDatabase(): Database =
+        Database(
+                id,
+                backdropPath,
+                character,
+                originalLanguage,
+                overview,
+                posterPath,
+                releaseDate,
+                title,
+                rating ?: 0.0
+        )
+
+fun Network.fromNetworkToDomain(): ActorListMovies =
+        ActorListMovies(
+                id, backdropPath, character, originalLanguage, overview, posterPath, releaseDate
+                ?: "", title, rating
+        )
