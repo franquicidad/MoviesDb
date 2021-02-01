596de0aa8e9ce34ceee3977bb5fdb0bdf636ca79
Felipe Franco <Felipe Franco>
2021-02-01 13:01:32 -0500

    Functions in airplane mode! everything working fine no bugs.2

--- app/src/main/java/com/franco/moviesdb/ui/actorsDetail/ActorsDetailFragment.kt
+++ app/src/main/java/com/franco/moviesdb/ui/actorsDetail/ActorsDetailFragment.kt
@@ -88,13 +88,7 @@ class ActorsDetailFragment : Fragment(R.layout.actors_detail_fragment) {
                     list.let {
                         it.collect {
                             Log.i("Any", "$it")
-//                            if (it.size == 0) {
-//                                recyclerMovieActor.visibility = View.GONE
-//                                noMoviesForActor.visibility = View.VISIBLE
-//                            } else {
-//                                recyclerMovieActor.visibility = View.VISIBLE
-//                                noMoviesForActor.visibility = View.GONE
-//                            }
+
 
                             movieListByActorAdapter.submitList(it)
                         }
